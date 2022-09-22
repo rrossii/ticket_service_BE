43e61cf00e657cd8803c38cdec15282a5c1f25cb
rosska <annros2003@gmail.com>
2022-09-23 01:02:23 +0300

    added path

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def firstHello():
+    return "Hello World!"
+
+
+@app.route("/localhost:5000/api/v1/hello-world-8")
 def hello():
-    return "Hello world 8"
+    return "<h2>Hello World! 8</h2>"
 
 
 if __name__ == "__main__":
