1f5d9109457d579779f6a78266535ffd9cabf6e2
rosska <annros2003@gmail.com>
2022-09-23 12:27:34 +0300

    add README.md

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def firstHello():
     return "Hello World!"
 
 
 @app.route("/localhost:5000/api/v1/hello-world-8")
 def hello():
-    return "<h2>Hello World! 8</h2>"
+    return "<h2 style='color:green'>Hello World! 8</h2>"
 
 
 if __name__ == "__main__":
